9dd6d75a22956271a68796fc25712f2ff76b36f3
Emirkcgl <yunus0598koca@gmail.com>
2026-02-23 16:06:08 +0300

    Implementing the Cities List

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,50 @@ import Product from "./pages/Product";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:9000";
+
 export default function App() {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchCities = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        console.log(data);
+        setCities(data);
+      } catch (error) {
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="/pricing" element={<Pricing />} />
         <Route path="/login" element={<Login />} />
-        <Route path="/app" element={<AppLayout />} />
+        <Route path="/app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isLoading={isLoading} />}
+          />
+          <Route path="countries" element={<p>Countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
+
         <Route path="/product" element={<Product />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
---
+++ src/components/CityItem.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import styles from "./CityItem.module.css";
+
+const formatDate = (date) =>
+  new Intl.DateTimeFormat("en", {
+    day: "numeric",
+    month: "long",
+    year: "numeric",
+    weekday: "long",
+  }).format(new Date(date));
+
+export default function CityItem({ city }) {
+  return (
+    <li className={styles.cityItem}>
+      <span className={styles.emoji}>{city.emoji}</span>
+      <span className={styles.name}>{city.cityName}</span>
+      <time className={styles.date}>{formatDate(city.date)}</time>
+      <button className={styles.deleteBtn}>&times;</button>
+    </li>
+  );
+}
