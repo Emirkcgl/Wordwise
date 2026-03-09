529d4d1015193a588f8c97ff77fa140dd14bd42f
Emirkcgl <yunus0598koca@gmail.com>
2026-03-09 16:15:43 +0300

    crateContext and consuming

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,38 @@
+import { createContext, useState, useEffect, useContext } from "react";
+const BASE_URL = "http://localhost:9000";
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchCities = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        console.log(data);
+        setCities(data);
+      } catch (error) {
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchCities();
+  }, []);
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (!context)
+    throw new Error("useCities must be used within a CitiesProvider");
+  return context;
+}
+
+export { CitiesProvider, useCities };
